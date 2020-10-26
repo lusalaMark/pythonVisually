310718a318331a66be17ace08e3908a6e0dc41d0
lusalaMark <lusalamark63@gmail.com>
2020-10-26 12:14:42 +0300

    functions

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import turtle
 
 mark_turtle = turtle.Turtle()
-mark_turtle.forward(200)
-mark_turtle.right(160)
-mark_turtle.forward(200)
-mark_turtle.left(160)
-mark_turtle.backward(200)
-mark_turtle.right(340)
-mark_turtle.forward(200)
+
+
+def parallelogram():
+    mark_turtle.forward(200)
+    mark_turtle.right(160)
+    mark_turtle.forward(200)
+    mark_turtle.left(160)
+    mark_turtle.backward(200)
+    mark_turtle.right(340)
+    mark_turtle.forward(200)
+
+parallelogram()
